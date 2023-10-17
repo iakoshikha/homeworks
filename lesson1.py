e4dea7b98b4549499bc088a2b161a4923d655d54
iakoshikha <148218296+iakoshikha@users.noreply.github.com>
2023-10-17 19:23:00 +0400

    Add files via upload

--- lesson1.py
+++ lesson1.py
@@ -5,7 +5,7 @@ from turtle import *
 
 shape ("turtle")
 #draw a square
-#speed(30)
+#speed(20)
 width(7)
 color("pink")
 forward (200)
@@ -46,4 +46,81 @@ forward(200)
 end_fill()
 
 
+#drawig a window
+width(4)
+color ("pink")
+
+penup()
+goto(120,200)
+pendown()
+
+left (30)
+forward (60)
+left(90)
+forward(79)
+left(90)
+forward (60)
+
+penup()
+goto(161,200)
+pendown()
+
+right(180)
+forward(57)
+
+penup()
+goto(161,175)
+pendown()
+
+right (90)
+forward (40)
+
+penup()
+goto(161,175)
+pendown()
+
+right(180)
+forward (39)
+
+penup()
+goto(85,200)
+pendown()
+
+right(90)
+forward(59)
+
+right(90)
+forward(82)
+
+right(90)
+forward(59)
+
+penup()
+goto(43,200)
+pendown()
+
+right(180)
+forward(59)
+
+penup()
+goto(43,175)
+pendown()
+
+right(90)
+forward(40)
+
+penup()
+goto(43,175)
+pendown()
+
+left(180)
+forward(40)
+
+penup()
+goto(110,60)
+pendown()
+
+
+
 exitonclick()
+
