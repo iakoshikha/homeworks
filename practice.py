fef92388135c63c371e78560c657a12590bfb524
iakoshikha <148218296+iakoshikha@users.noreply.github.com>
2023-10-24 00:39:37 +0400

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,34 @@
+name = "iako"
+
+# #i want make a homework 
+
+# print(name)
+
+# print(name.upper())
+
+# print(name.strip())
+
+smthboutme = """i am 20 years old and i'm 
+ begginer in this industry,
+ wish me luck"""
+
+# print(smthboutme)
+
+# print(smthboutme.replace("i","$"))
+
+# if 10>5:
+#  print(smthboutme)
+
+print(len(smthboutme))
+# print(smthboutme[2:5])
+# print(smthboutme[2:45:5])
+
+# print(smthboutme[:])
+
+print(smthboutme[-5])
+
+
+
+
+
+
