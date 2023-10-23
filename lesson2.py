47baf0b7ae22895411bf5adfd77482d2f081fc24
iakoshikha <148218296+iakoshikha@users.noreply.github.com>
2023-10-23 16:00:52 +0400

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,65 @@
+name = "iako"
+
+print (len(name))
+   #4
+
+my_sentence = """გამარჯობა,რომ სწავლას იწყებთ 
+19 სექტემბერს, 
+გთხოვთ არ გააცდინოთ"""
+
+print (my_sentence)
+
+knows_programming = True
+
+if 10>5:
+
+ print("hello")
+   #tab-ის საშუალებით მოვახდენთ ინდენტაციას 
+
+#full_name = "iako shikhashvili"  #4+12=16=Tab  
+#print (len(full_name))
+
+#ყველა სტრინგი შეგვიძლია მივიჩნიოთ სიად
+
+#print(full_name[5])
+
+#position = index სინონიმებია
+
+#print("k" in full_name)
+
+full_name = "iako shikhashvili"
+# print(full_name[2:9])
+# print(full_name[2:9:2])
+# #start.finit.step
+
+# print(full_name[:5])
+# print(full_name[5:])
+
+# print(full_name[:])
+
+# print(full_name[-1:-6:-1])
+
+#სტრინგებს აქვთ მეთოდები
+#  1) string. upper
+# 2) len(string)
+
+   #თუ ვერ გავიგე google (len method python)
+
+print(full_name.upper())
+
+name2 = "  IAKO   "
+
+print(name2.lower())
+print(name2)
+print(name2.strip())
+
+#1. space gareshe
+#2. space-stan ertad
+
+print(name2.replace(" ", "#"))
+
+full_name2 = "iako shikashvili"
+print(full_name2)
+
+print(full_name2.replace("i", "#"))
+
