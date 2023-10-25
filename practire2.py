c1c406b060e22d3367de91e2a5bd60cf55a5cd85
iakoshikha <148218296+iakoshikha@users.noreply.github.com>
2023-10-26 02:25:10 +0400

    Add files via upload

---
+++ practire2.py
@@ -0,0 +1,42 @@
+
+# item = "laptop"
+
+# price = 100
+# print ("i have a "+ item "and it costs")
+
+# print(24)
+
+# print(7+5)
+# print("7"+"10")
+
+
+# points = 45
+
+
+# name = "Tom"
+# level = 14
+# print(name)
+# level = level + 1
+# print(level)
+
+# player = "James"
+# level = 14
+
+
+mom_age = 47
+dad_age = 50
+brother_age = 26
+cat_age = 3
+
+print(dad_age)
+
+print(dad_age+mom_age+brother_age)
+
+print(mom_age,dad_age,brother_age,cat_age)
+
+print(mom_age+5)
+print(dad_age+5,brother_age+5,cat_age+5)
+
+print((dad_age,brother_age,cat_age)+5)
+
+
