3dd0645b3899497c6556ba235f4184c4eefcb4ba
iakoshikha <148218296+iakoshikha@users.noreply.github.com>
2023-11-09 23:26:08 +0400

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,6 +1,29 @@
-name = (input("enter your name : "))
-username = (input("enter ur urename : "))
-age = (input("enter your age: "))
+userage = int(input("enter your dad's age: "))
 
-age = int(input("enter your age: "))
-print (age+25)
+userdadage= int(input("enter your age: "))
+
+
+print(userage, userdadage)
+
+for years in range(1, 21):
+    userage =+ 1
+    userdadage += 1
+
+    differenceAge = userdadage // userage
+
+    # // -/ მთელებს აჭრის და ზუსტი გამოაქვს
+#  რამდენი იტერაცია  გვინდა რომ მოხდეს
+
+
+
+    print("in" , years, "your father will be", differenceAge, "years older than you")
+
+
+
+# target_year = 2030
+
+# for year in range(2023, target_year + 1) :
+#     age_in_that_year = year - 1973 + age
+#     my_age_in_that_year = year - 2003 + my_age
+#     print("in", year "you will be", my_age_in_that_year ")
+   
---
+++ homework2.py
@@ -0,0 +1,8 @@
+
+
+for number in range (1, 30, 2):
+  print(number)
+
+for number in range (2, 30, 2):
+  print(number)www
+
