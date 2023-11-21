ed44fb4e68da3e30aa184518a37adb930f5c1a0e
iakoshikha <148218296+iakoshikha@users.noreply.github.com>
2023-11-21 18:24:56 +0400

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,9 +1,13 @@
-age = 35
-if age > 9:
-    print("გილოცავ მატრიცელო შენ გადმოგეცემა  3000 ლარიანი ბანძი ტოსტერი რისთვისაც შეალიე შენი ცხოვრების წლები")
-elif age < 5 :
-    print("გილოცავ გაექეცი მატრიცას")
-elif age >=5 and age <= 9 :
-    print("you are mid")
-else:
-    print("there is something wrong with you")
+my_arr = (2,5,16,18,28,17,39,75)
+
+my_sum = 0
+for num in my_arr:
+    my_sum =+ num
+    print(my_sum)
+
+    
+for num in my_arr:
+    if num % 2 == 0 :
+        print (srt(num)) + "ლუწი"
+    else:
+        print (srt(num)) + "კენტი"
